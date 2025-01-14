e1e50fea9ff5126fdd098add489468ef668feee8
genesis <gmluch@scl.edu.gt>
2025-01-14 11:35:42 -0600

    primerejercicio

---
+++ main.js
@@ -0,0 +1,35 @@
+//alert("Hola mundo") Crear alerta
+
+//VARIABLES
+let edad = 18; //Datos en memoria
+let nombre = "Genesis";
+let decimal = 1.2;  //let : forma de crear variables
+
+const PI = 3.1416;
+
+//Ejercicio
+let objeto_de_trabajo = "computadora";
+let cantidad_de_computadoras = 22;
+let cantidad_de_alumnos = 21;
+let lamparas = 4;
+let audiovisual = "Televisión"
+let mesas = 11;
+let nombre_profe = "Jossue";
+let pizarron = 1;
+let cortinas = 1;
+let altura_promedio_alumnos = 1.65;
+
+
+//Tarea
+
+//Primer enunciado
+let tiempo = 10; //10s
+let velocidad_inicial = 0;
+let velocidad_final = 30;
+
+
+//Segundo enunciado
+let lote = 150; //Productos
+let precio_productos = 50; //Quetzales
+let se_venden_a = 75; //Por unidad
+
